d85c68af34f04a98726e408e5663917485b0f68c
Gaurav Suresh Gavhane <gauravgavhane1702@gmail.com>
2025-06-17 18:04:12 +0530

    sequence data type

--- sequence.py
+++ sequence.py
@@ -1,7 +1,7 @@
 print("Demonstration of sequence data types")
 
 ldata = [10,20,30,40]               # List
-tdata = (10,20,30,40)               # Touple
+tdata = (10,20,30,40)               # Tuple
 sdata = {10,20,30,40}               # Set
 ddata = {"C":400, "C++":550, "Java":900, "Python" : 890}    # Dictinary
 
